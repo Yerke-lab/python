16ec42895ee0886c3923b23ea1e71b39c88ac274
Yerke-lab <110925046+Yerke-lab@users.noreply.github.com>
2022-10-09 19:05:38 +0600

    домашние задания 2 и 3 по семинару 2
    добавление ДЗ

---
+++ sem_2_2.py
@@ -0,0 +1,14 @@
+#Напишите программу, которая принимает на вход число N
+# и выдает набор произведений чисел от 1 до N.
+#Пример:
+#- пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+from math import factorial
+
+n = int(input('Введите число: '))
+lst = []
+factorial = 1
+for i in range(1, n + 1):
+    factorial *= i
+    lst.append(factorial)
+print(lst)
